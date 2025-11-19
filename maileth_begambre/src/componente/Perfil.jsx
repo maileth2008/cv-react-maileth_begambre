898872c80dfd3cf52fbc500845908e6d72f256b5
MMBEGAMBRE <mailethbegambre615@gmail.com>
2025-11-19 00:57:54 +0000

    componente Perfil con descripción personalizada

---
+++ maileth_begambre/src/componente/Perfil.jsx
@@ -0,0 +1,11 @@
+function Perfil() {
+  return (
+    <section>
+      <h2>Perfil</h2>
+      <p>Soy aprendiz del SENA, con interés en el desarrollo web moderno utilizando React.</p>
+      <p>Responsable, creativa y con habilidades de trabajo en equipo.</p>
+    </section>
+  );
+}
+
+export default Perfil;
