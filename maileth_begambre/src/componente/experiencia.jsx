73f3acb3e9ca5e1861dc428461f2fb5e14b88368
MMBEGAMBRE <mailethbegambre615@gmail.com>
2025-11-19 01:13:16 +0000

    componente Experiencia con 4 cargos definidos

---
+++ maileth_begambre/src/componente/experiencia.jsx
@@ -0,0 +1,33 @@
+function Experiencia() {
+  const experiencias = [
+    {
+      cargo: "Frontend Junior - Proyecto Personal",
+      empresa: "Independiente",
+      año: "2025",
+      descripcion: "Desarrollo de interfaces funcionales con React y Vite.",
+    },
+    {
+      cargo: "Soporte Técnico",
+      empresa: "SENA",
+      año: "2024",
+      descripcion: "Atención a usuarios y mantenimiento básico de equipos.",
+    },
+    {
+      cargo: "Proyecto con Python",
+      empresa: "Independiente",
+      año: "2025",
+      descripcion: "Programas para resolver problemas matemáticos.",
+    },
+    {
+      cargo: "Diseñador Web Freelance",
+      empresa: "Freelance",
+      año: "2025",
+      descripcion: "Creación de sitios web con HTML y CSS.",
+    },
+  
+  ];
+
+  
+}
+
+export default Experiencia;
