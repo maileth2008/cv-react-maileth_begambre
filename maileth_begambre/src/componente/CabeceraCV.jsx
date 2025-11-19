5f93b9a78aba44074d976c769e2883743ccd459e
MMBEGAMBRE <mailethbegambre615@gmail.com>
2025-11-19 00:50:45 +0000

    componente CabeceraCV con datos personales actualizados

---
+++ maileth_begambre/src/componente/CabeceraCV.jsx
@@ -0,0 +1,17 @@
+function CabeceraCV() {
+  return (
+    <header>
+      <h1>
+  <span className="rosa">Maileth</span>{" "}
+  <span className="negro">Begambre</span>
+</h1>
+
+      <h2>Aprendiz SENA</h2>
+      <p> 3140279737</p>
+      <p>mailethbegambre615@gmail.com</p>
+      <p>Medellín, Colombia</p>
+    </header>
+  );
+}
+
+export default CabeceraCV;
