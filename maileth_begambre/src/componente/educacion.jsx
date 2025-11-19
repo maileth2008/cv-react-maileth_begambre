085877d27bc0a9c6a5abf0e0955f85438ba19724
MMBEGAMBRE <mailethbegambre615@gmail.com>
2025-11-19 01:16:47 +0000

    componente Educacion con formación complementaria

---
+++ maileth_begambre/src/componente/educacion.jsx
@@ -0,0 +1,14 @@
+function Educacion() {
+  return (
+    <section>
+      <h2>Educación</h2>
+
+      <p>Tecnólogo en Análisis y Desarrollo de Software - SENA (En curso)</p>
+      <p>Bachiller Técnico - I.E Técnico Agropecuario Claret (2024)</p>
+      <p>Curso: Fundamentos de Programación  -SENA 2025</p>
+      <p>Curso: ingles basico -sena 2025</p>
+    </section>
+  );
+}
+
+export default Educacion;
